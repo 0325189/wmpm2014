1844b4dceed22b338ce904e4b69817681f582bd2
0325189 <e0325189@student.tuwien.ac.at>
2014-05-13 13:05:25 +0200

    FlightFinder Maven Project added

---
+++ wmpm2014-flightfinder/WMPM2014-FlightFinder/src/main/java/at/tuwien/flightfinder/config/FlightFinderConfig.java
@@ -0,0 +1,35 @@
+package at.tuwien.flightfinder.config;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.spring.SpringCamelContext;
+import org.apache.camel.spring.javaconfig.CamelConfiguration;
+import org.apache.camel.spring.javaconfig.Main;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FlightFinderConfig extends CamelConfiguration implements InitializingBean {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws Exception {
+		new Main().run(args);
+	}
+	
+	/**
+    * Returns the CamelContext which support Spring
+    */
+	protected CamelContext createCamelContext() throws Exception {
+		return new SpringCamelContext(getApplicationContext());
+	}
+	
+    protected void setupCamelContext(CamelContext camelContext) throws Exception {
+        // nothing here :( yet :)
+    }
+	
+	public void afterPropertiesSet() throws Exception {
+        // just to make Spring happy do nothing here
+    }
+
+}
