ed793fbf3a9cd230873e6bd48fd38db2ad819421
MS-1128233 <e1128233@student.tuwien.ac.at>
2014-05-15 00:41:19 +0200

    fileOffers - activeMQ added

--- wmpm2014-flightfinder/WMPM2014-FlightFinder/src/main/java/at/tuwien/flightfinder/config/FlightFinderConfig.java
+++ wmpm2014-flightfinder/WMPM2014-FlightFinder/src/main/java/at/tuwien/flightfinder/config/FlightFinderConfig.java
@@ -1,6 +1,10 @@
 package at.tuwien.flightfinder.config;
 
+import javax.jms.ConnectionFactory;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.camel.CamelContext;
+import org.apache.camel.component.jms.JmsComponent;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.apache.camel.spring.SpringCamelContext;
 import org.apache.camel.spring.javaconfig.CamelConfiguration;
@@ -17,32 +21,35 @@ public class FlightFinderConfig {
 	public static void main(String[] args) throws Exception {
 		CamelContext context = new DefaultCamelContext();
 		context.addRoutes(new FtpRouteConfig());
-		System.out.println("hallowordl");
+
+		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost");	
+		context.addComponent("jms",JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
+
 		context.start();
 		System.out.println("CamelContext started");
-		
+
 		// wait for 30 seconds (in which the route will watch out for the endpoint)
 		Thread.sleep(30000);
-		
+
 		context.stop();
 		System.out.println("CamelContext stopped");
-		
+
 	}
-	
-//	/**
-//    * Returns the CamelContext which support Spring
-//    */
-//	protected CamelContext createCamelContext() throws Exception {
-//		return new SpringCamelContext(getApplicationContext());
-//	}
-//	
-//    protected void setupCamelContext(CamelContext camelContext) throws Exception {
-//        // nothing here :( yet :)
-	
-//    }
-//	
-//	public void afterPropertiesSet() throws Exception {
-//        // just to make Spring happy do nothing here
-//    }
+
+	//	/**
+	//    * Returns the CamelContext which support Spring
+	//    */
+	//	protected CamelContext createCamelContext() throws Exception {
+	//		return new SpringCamelContext(getApplicationContext());
+	//	}
+	//	
+	//    protected void setupCamelContext(CamelContext camelContext) throws Exception {
+	//        // nothing here :( yet :)
+
+	//    }
+	//	
+	//	public void afterPropertiesSet() throws Exception {
+	//        // just to make Spring happy do nothing here
+	//    }
 
 }
--- wmpm2014-flightfinder/WMPM2014-FlightFinder/src/main/java/at/tuwien/flightfinder/config/FtpRouteConfig.java
+++ wmpm2014-flightfinder/WMPM2014-FlightFinder/src/main/java/at/tuwien/flightfinder/config/FtpRouteConfig.java
@@ -7,9 +7,10 @@ import org.apache.camel.builder.RouteBuilder;
 
 public class FtpRouteConfig extends RouteBuilder{
 	@Override
-	public void configure() throws Exception {
+	public void configure() throws Exception {	
 		from("ftp://ftp6291381_workflow@www92.world4you.com?password=workflow2014&consumer.delay=60000")
-		.process(new FtpRouteLogger()).to("file:data/outbox");
+		.validate(header("CamelFileName").regex("^.*(csv|csl)$"))
+		.process(new FtpRouteLogger()).to("jms:topic:fileOffers");
 	}
 
 
