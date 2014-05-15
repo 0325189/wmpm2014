a933012e3b7e90b27dce360d8e5b82b14362f756
0325189 <e0325189@student.tuwien.ac.at>
2014-05-15 03:32:36 +0200

    Annotated FtpRoute class

--- wmpm2014-flightfinder/WMPM2014-FlightFinder/src/main/java/at/tuwien/flightfinder/routes/FtpRouteConfig.java
+++ wmpm2014-flightfinder/WMPM2014-FlightFinder/src/main/java/at/tuwien/flightfinder/routes/FtpRouteConfig.java
@@ -1,11 +1,13 @@
 package at.tuwien.flightfinder.routes;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
 
 import at.tuwien.flightfinder.beans.FtpRouteLogger;
 
-
+@Component
 public class FtpRouteConfig extends RouteBuilder{
+	
 	@Override
 	public void configure() throws Exception {	
 		from("ftp://ftp6291381_workflow@www92.world4you.com?password=workflow2014&consumer.delay=60000")
