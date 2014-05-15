988dd05434f50c29331a25e92884abbbdb3a4aae
0325189 <e0325189@student.tuwien.ac.at>
2014-05-15 03:20:08 +0200

    Changed FTP Route to use ActiveMQ component and queue

--- wmpm2014-flightfinder/WMPM2014-FlightFinder/src/main/java/at/tuwien/flightfinder/config/FtpRouteConfig.java
+++ wmpm2014-flightfinder/WMPM2014-FlightFinder/src/main/java/at/tuwien/flightfinder/config/FtpRouteConfig.java
@@ -10,7 +10,7 @@ public class FtpRouteConfig extends RouteBuilder{
 	public void configure() throws Exception {	
 		from("ftp://ftp6291381_workflow@www92.world4you.com?password=workflow2014&consumer.delay=60000")
 		.validate(header("CamelFileName").regex("^.*(csv|csl)$"))
-		.process(new FtpRouteLogger()).to("jms:topic:fileOffers");
+		.process(new FtpRouteLogger()).to("activemq:fileOffers");
 	}
 
 
