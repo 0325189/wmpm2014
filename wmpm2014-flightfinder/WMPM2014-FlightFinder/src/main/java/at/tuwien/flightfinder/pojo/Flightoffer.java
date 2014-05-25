dedbf2c4018042b1fc22b3e2134d26263a44b1cf
IG-1029600 <e1029600@student.tuwien.ac.at>
2014-05-25 16:08:58 +0200

    hibernate

---
+++ wmpm2014-flightfinder/WMPM2014-FlightFinder/src/main/java/at/tuwien/flightfinder/pojo/Flightoffer.java
@@ -0,0 +1,111 @@
+package at.tuwien.flightfinder.pojo;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+@Entity
+public class Flightoffer {
+	@Id
+	@GeneratedValue
+	private long id;
+	@ManyToOne(cascade=CascadeType.ALL, fetch=FetchType.EAGER)
+	@JoinColumn(name="from_id")
+	private Airport fromAirport;
+	@ManyToOne(cascade=CascadeType.ALL,fetch=FetchType.EAGER)
+	@JoinColumn(name="to_id")
+	private Airport toAirport;
+	private int flightNumber;
+	private Date flightDate;
+	private int ticketId;
+	private Double price;
+	@Enumerated(EnumType.STRING)
+	private FlightClass flightClass;
+	@OneToMany(cascade=CascadeType.ALL,mappedBy="flightoffer",fetch=FetchType.EAGER)
+	private List<Hotel> hotels;
+
+
+	public Flightoffer(int flightNumber, Date flightDate, int ticketId,
+			Double price) {
+		this.flightNumber = flightNumber;
+		this.flightDate = flightDate;
+		this.ticketId = ticketId;
+		this.price = price;
+	}
+	Flightoffer(){	
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public int getFlightNumber() {
+		return flightNumber;
+	}
+
+	public void setFlightNumber(int flightNumber) {
+		this.flightNumber = flightNumber;
+	}
+
+	public Date getFlightDate() {
+		return flightDate;
+	}
+
+	public void setFlightDate(Date flightDate) {
+		this.flightDate = flightDate;
+	}
+
+	public int getTicketId() {
+		return ticketId;
+	}
+
+	public void setTicketId(int ticketId) {
+		this.ticketId = ticketId;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	public Airport getFromAirport() {
+		return fromAirport;
+	}
+	public void setFromAirport(Airport fromAirport) {
+		this.fromAirport = fromAirport;
+	}
+	public Airport getToAirport() {
+		return toAirport;
+	}
+	public void setToAirport(Airport toAirport) {
+		this.toAirport = toAirport;
+	}
+	public FlightClass getFlightClass() {
+		return flightClass;
+	}
+	public void setFlightClass(FlightClass flightClass) {
+		this.flightClass = flightClass;
+	}
+	public List<Hotel> getHotels() {
+		return hotels;
+	}
+	public void setHotels(List<Hotel> hotels) {
+		this.hotels = hotels;
+	}
+
+}
