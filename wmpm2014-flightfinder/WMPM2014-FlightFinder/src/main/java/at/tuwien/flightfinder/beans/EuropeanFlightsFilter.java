ca0318c4b67b466be17a4cb05181fc4c47947a89
Sanjin Becirovic <sanyobecirovic@gmail.com>
2014-05-26 20:37:59 +0200

    EuropeanFlightsFilter

--- wmpm2014-flightfinder/WMPM2014-FlightFinder/src/main/java/at/tuwien/flightfinder/beans/EuropeanFlightsFilter.java
+++ wmpm2014-flightfinder/WMPM2014-FlightFinder/src/main/java/at/tuwien/flightfinder/beans/EuropeanFlightsFilter.java
@@ -1,6 +1,10 @@
 package at.tuwien.flightfinder.beans;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import org.apache.camel.*;
 import org.apache.camel.builder.xml.XPathBuilder;
@@ -9,24 +13,45 @@ import static org.apache.camel.builder.PredicateBuilder.not;
 
 public class EuropeanFlightsFilter implements Predicate {
 
+
 	ArrayList<String> europeanFlightList = new ArrayList();
-	
+
 	@Override
 	public boolean matches(Exchange exchange) {
-		
-		 	String origin = XPathBuilder.xpath("//Origin").evaluate(exchange, String.class);
-		 	europeanFlightList.add("VIE");
-		 	
-		 	System.out.println(origin);
-		 	
-			// boolean matches = XPathBuilder.xpath("Origin").matches(exchange);
-			
-			if (europeanFlightList.contains(origin)){
-				System.out.println(origin);
-				return true;
+
+		Scanner scanner;
+		try 
+		{
+			scanner = new Scanner(new File("mojTest/IATAEuropeanCodes.csv"));
+			scanner.useDelimiter("/n");
+
+			while (scanner.hasNext())
+			{
+				europeanFlightList.add(scanner.next());
+				System.out.println(scanner.next());
 			}
-				
+
+			scanner.close();
+
+		} 
+		catch (FileNotFoundException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		String origin = XPathBuilder.xpath("//Origin").evaluate(exchange, String.class);
+
+		System.out.println(origin);
+
+		// boolean matches = XPathBuilder.xpath("Origin").matches(exchange);
+
+		if (europeanFlightList.contains(origin)){
+			System.out.println(origin);
+			return true;
+		}
+
 		return false;
 	}
-		
+
 }
