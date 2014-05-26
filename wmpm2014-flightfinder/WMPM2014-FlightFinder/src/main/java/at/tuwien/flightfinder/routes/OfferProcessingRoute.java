06a366838535474e36cf31ea2b4c64604ca54368
MS-1128233 <e1128233@student.tuwien.ac.at>
2014-05-26 23:27:46 +0200

    OffersEnricher (structure)

--- wmpm2014-flightfinder/WMPM2014-FlightFinder/src/main/java/at/tuwien/flightfinder/routes/OfferProcessingRoute.java
+++ wmpm2014-flightfinder/WMPM2014-FlightFinder/src/main/java/at/tuwien/flightfinder/routes/OfferProcessingRoute.java
@@ -5,13 +5,13 @@ import org.springframework.stereotype.Component;
 import org.apache.camel.component.hazelcast.*;
 
 import at.tuwien.flightfinder.beans.EuropeanFlightsFilter;
+import at.tuwien.flightfinder.beans.OffersEnricher;
 
 
 @Component
 public class OfferProcessingRoute extends RouteBuilder {
 
 	//EuropeanFlightsFilter FlightFinderFilteringRules = new EuropeanFlightsFilter();
-
 	@Override
 	public void configure() throws Exception {
 
@@ -19,31 +19,17 @@ public class OfferProcessingRoute extends RouteBuilder {
 		log("${body}").
 		log("Message has been pulled from Offers queue").
 		filter(new EuropeanFlightsFilter()).
-		to("activemq:FilterOffer").
+		process(new OffersEnricher()).
+
+		//to("activemq:FilterOffer").
 		log("${body}").
-		log("Filtered messages beeing pushed to FilterOffer Queue")
-//		.pollEnrich("activemq:Offers",new AggregationStrategy(){
-//			public Exchange aggregate(Exchange oldExchange,Exchange newExchange) {
-//				if (newExchange == null) {
-//					return oldExchange;
-//				}
-//
-//				String fromQueue = oldExchange.getIn()
-//						.getBody(String.class);
-//				String fromDB = newExchange.getIn()
-//						.getBody(String.class);
-//				//find the airport destination in the old message, check the parser in filter class
-//				//retrieve getFlightofferById(int offerId) and save it(I need FlightofferID for that, how to get it??)
-//				//use getI
-//				String body = fromQueue + "\n" + fromDB;
-//				oldExchange.getIn().setBody(body);
-//				return oldExchange;
-//			}
-//		})
+		log("Filtered messages beeing pushed to FilterOffer Queue").
 		//.to("activemq:EnrichedOffer")
+		to("file:mojTest?fileName=test.xml");
+
 		//.wireTap("jms:orderAudit")//use hibernate instead of jms
-		.setHeader(HazelcastConstants.OPERATION, constant(HazelcastConstants.PUT_OPERATION))
-		.toF("hazelcast:seda:promotionQueue", HazelcastConstants.QUEUE_PREFIX);
+		//.setHeader(HazelcastConstants.OPERATION, constant(HazelcastConstants.PUT_OPERATION))
+		//.toF("hazelcast:seda:promotionQueue", HazelcastConstants.QUEUE_PREFIX);
 	}
 
 }
