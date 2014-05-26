97da99335ce747399a5bf5b4d15d6b3b170c2f73
MS-1128233 <e1128233@student.tuwien.ac.at>
2014-05-26 18:45:44 +0200

    EuropeanFlightFilter - now loading list of acceptable destinations from csv file.

--- wmpm2014-flightfinder/WMPM2014-FlightFinder/src/main/java/at/tuwien/flightfinder/beans/EuropeanFlightsFilter.java
+++ wmpm2014-flightfinder/WMPM2014-FlightFinder/src/main/java/at/tuwien/flightfinder/beans/EuropeanFlightsFilter.java
@@ -1,32 +1,46 @@
 package at.tuwien.flightfinder.beans;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import org.apache.camel.*;
 import org.apache.camel.builder.xml.XPathBuilder;
 
+import au.com.bytecode.opencsv.CSVReader;
 import static org.apache.camel.builder.PredicateBuilder.not;
 
 public class EuropeanFlightsFilter implements Predicate {
-
 	ArrayList<String> europeanFlightList = new ArrayList();
-	
 	@Override
 	public boolean matches(Exchange exchange) {
-		
-		 	String origin = XPathBuilder.xpath("//Origin").evaluate(exchange, String.class);
-		 	europeanFlightList.add("VIE");
-		 	
-		 	System.out.println(origin);
-		 	
-			// boolean matches = XPathBuilder.xpath("Origin").matches(exchange);
-			
-			if (europeanFlightList.contains(origin)){
-				System.out.println(origin);
-				return true;
+		Scanner sc;
+
+		try {
+			sc = new Scanner(new File("mojTest/EuropeanCities.csv"));
+			sc.useDelimiter("\n");
+			while(sc.hasNext()){
+				europeanFlightList.add(sc.nextLine());
 			}
-				
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+
+		String origin = XPathBuilder.xpath("//NameOrigin").evaluate(exchange, String.class);
+		String trimedOrigin = origin.substring(origin.indexOf('>')+1,origin.lastIndexOf('<'));
+		//System.out.println("What is my origin: "+trimedOrigin);
+
+		// boolean matches = XPathBuilder.xpath("Origin").matches(exchange);
+
+		if (europeanFlightList.contains(trimedOrigin)){
+			System.out.println("The destination "+trimedOrigin+" has been accepted by the filter!");
+			return true;
+		}
+
 		return false;
 	}
-		
+
 }
