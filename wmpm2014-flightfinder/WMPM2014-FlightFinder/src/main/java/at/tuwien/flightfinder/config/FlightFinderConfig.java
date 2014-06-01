50ffa4154ec14ed09833116039fca9ac94f824a7
0325189 <e0325189@student.tuwien.ac.at>
2014-06-01 18:12:26 +0200

    Hazelcast removed; Changes to FlightFinderApp and Configuration;

--- wmpm2014-flightfinder/WMPM2014-FlightFinder/src/main/java/at/tuwien/flightfinder/config/FlightFinderConfig.java
+++ wmpm2014-flightfinder/WMPM2014-FlightFinder/src/main/java/at/tuwien/flightfinder/config/FlightFinderConfig.java
@@ -3,21 +3,15 @@ package at.tuwien.flightfinder.config;
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.camel.component.ActiveMQComponent;
 import org.apache.camel.CamelContext;
-import org.apache.camel.component.hazelcast.HazelcastComponent;
 import org.apache.camel.spring.SpringCamelContext;
 import org.apache.camel.spring.javaconfig.CamelConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import com.hazelcast.config.Config;
-import com.hazelcast.core.Hazelcast;
-import com.hazelcast.core.HazelcastInstance;
-
 /**
  * This is where SpringCamelContext is configured WITHOUT any Spring XML stuff :)
  * ComponentScan will scan for all classes annotated with "Component" in the defined 
  * package. Every class containing a Camel route should be annotated and extend RouteBuilder
- * 
  */
 
 @Configuration
@@ -42,12 +36,6 @@ public class FlightFinderConfig extends CamelConfiguration {
 		ActiveMQComponent activeMQcomp = new ActiveMQComponent();
 		activeMQcomp.setConnectionFactory(connectionFactory);
 		camelContext.addComponent("activemq", activeMQcomp);
-				
-		Config cfgHazelcast = new Config();
-		HazelcastInstance hzInstance = Hazelcast.newHazelcastInstance(cfgHazelcast);
-		HazelcastComponent hzComponent = new HazelcastComponent();
-		hzComponent.setHazelcastInstance(hzInstance);
-		camelContext.addComponent("hazelcast", hzComponent);
     
     }
 
