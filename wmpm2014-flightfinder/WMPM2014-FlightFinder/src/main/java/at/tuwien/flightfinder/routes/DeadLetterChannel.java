e026561c23df9b023ed6353b3bcd0bc86876aeb9
0325189 <e0325189@student.tuwien.ac.at>
2014-06-22 10:47:29 +0200

    DeadLetterChannel implemented

---
+++ wmpm2014-flightfinder/WMPM2014-FlightFinder/src/main/java/at/tuwien/flightfinder/routes/DeadLetterChannel.java
@@ -0,0 +1,23 @@
+package at.tuwien.flightfinder.routes;
+
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+import org.apache.log4j.Logger;
+
+/*
+ * .errorHandler(deadLetterChannel(DeadLetterChannel.DLCH))
+ */
+
+@Component
+public class DeadLetterChannel extends RouteBuilder {
+
+	private static final Logger log = Logger.getLogger(DeadLetterChannel.class);
+	public static final String DLCH = "activemq:dead-letter";
+	
+	@Override
+	public void configure() throws Exception {
+		
+		from(DLCH).log(LoggingLevel.INFO, "Message in dead letter queue");
+	}
+}
