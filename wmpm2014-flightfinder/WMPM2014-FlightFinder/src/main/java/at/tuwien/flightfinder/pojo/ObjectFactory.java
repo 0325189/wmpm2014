8c1da0b8d3163dfc97e1c9854e71578355d99d17
MS-1128233 <e1128233@student.tuwien.ac.at>
2014-07-10 10:16:49 +0200

    objectfactory

---
+++ wmpm2014-flightfinder/WMPM2014-FlightFinder/src/main/java/at/tuwien/flightfinder/pojo/ObjectFactory.java
@@ -0,0 +1,10 @@
+package at.tuwien.flightfinder.pojo;
+import javax.xml.bind.annotation.XmlRegistry;
+
+@XmlRegistry
+public class ObjectFactory {
+
+	public static Flightoffer createFlightoffers() {
+        return new Flightoffer();
+    }
+}
